eb074e2c83695aa191b30714b6a373f5e54c9ff2
html546 <1776787598@qq.com>
2018-02-18 22:18:12 +0800

    goto3

--- goto.php
+++ goto.php
@@ -6,15 +6,25 @@
  * Time: 22:06
  */
 
-for ($i = 0,$j = 50;$i<100;$i++){
-    while ($j--){
-        if($j == 17){
-            goto end;
-        }else{
-            echo "变量i = {$i},变量j = {$j}<br/>";
-        }
-    }
+$var = 2;
+switch ($var){
+    case 1:
+        goto one;
+        echo "one";
+    case 2:
+        goto two;
+        echo "two";
+    case 3:
+        goto three;
+        echo "three";
 }
 
-echo "i = $i";
-end :
+one:
+    echo "如果变量的值是1，将跳转到此处执行!";
+    exit;
+two:
+    echo "如果变量的值是2，将跳转到此处执行!";
+    exit;
+three:
+    echo "如果变量的值是3,将跳转到此处执行!";
+    exit;
