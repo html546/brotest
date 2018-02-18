6f9e9608d70e435a0108f3be83e27ce7693ca767
html546 <1776787598@qq.com>
2018-02-18 16:23:44 +0800

    计算器1

---
+++ calc.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP实现简易计算器(使用分支结构)</title>
+</head>
+<body>
+<?php
+    $mess = '';
+    if(isset($_POST['sub'])){
+        if($_POST['num1'] == ''){
+            $mess .= '第一个数不能为空<br/>';
+        }elseif (!is_numeric($_POST['num1'])){
+            $mess .= '第一个数必须是数字!<br/>';
+        }
+        if($_POST['num2'] == ''){
+            $mess .= '第二个数不能为空<br/>';
+        }elseif (!is_numeric($_POST['num2'])){
+            $mess .= '第二个数必须是数字!<br/>';
+        }elseif ($_POST['opt'] == '/'&& $_POST['num2'] == 0){
+            $mess .= '除数不能为0';
+        }
+    }
+?>
+<table border="1" align="center" width="400">
+    <form action="" method="post">
+        <caption><h1>简易计算器</h1></caption>
+        <tr>
+            <td>
+                <input type="text" size="4" name="num1" value="<?php echo @$_POST['num1']?>" />
+            </td>
+            <td>
+                <select name="opt">
+                    <option value="+" <?php echo @$_POST["opt"] == "+" ? "selected":"" ?>>+</option>
+                    <option value="-" <?php echo @$_POST["opt"] == "-" ? "selected":"" ?>>-</option>
+                    <option value="*" <?php echo @$_POST["opt"] == "*" ? "selected":"" ?>>*</option>
+                    <option value="/" <?php echo @$_POST["opt"] == "/" ? "selected":"" ?>>/</option>
+                    <option value="%" <?php echo @$_POST["opt"] == "%" ? "selected":"" ?>>%</option>
+                </select>
+            </td>
+            <td>
+                <input type="text" size="4" name="num2" value="<?php echo @$_POST["num2"] ?>">
+            </td>
+            <td>
+                <input type="submit" name="sub" value="计算">
+            </td>
+        </tr>
+    </form>
+    <?php
+        if(isset($_POST['sub'])){
+            echo "<tr><td colspan='4'>";
+            if(!$mess){
+                $sum = 0;
+                switch ($_POST['opt']){
+                    case '+':
+                        $sum = $_POST['num1'] + $_POST['num2'];
+                        break;
+                    case '-':
+                        $sum = $_POST['num1'] - $_POST['num2'];
+                        break;
+                    case '*':
+                        $sum = $_POST['num1'] * $_POST['num2'];
+                        break;
+                    case '/':
+                        $sum = $_POST['num1'] / $_POST['num2'];
+                        break;
+                    case '%':
+                        $sum = $_POST['num1'] % $_POST['num2'];
+                        break;
+                }
+                echo "结果: {$_POST['num1']} {$_POST['opt']} {$_POST['num2']} = {$sum} ";
+            }else{
+                echo $mess;
+            }
+            echo "</td></tr>";
+        }
+    ?>
+</table>
+</body>
+</html>
