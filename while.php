88acabffda8cd9801435054a2c2230b0f3078fc4
html546 <1776787598@qq.com>
2018-02-18 17:23:14 +0800

    使用while循环嵌套输出表格

---
+++ while.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>使用while循环嵌套输出表格</title>
+</head>
+<body>
+    <table align="center" border="1" width="400">
+        <caption><h1>使用while循环嵌套输出表格</h1></caption>
+        <?php
+        $out = 0;
+
+        while($out<10){
+            $bgcolor = $out % 2 == 0 ? "#fff":"#ddd";
+            echo "<tr bgcolor=".$bgcolor.">";
+                $in = 1;
+                while ($in <= 10){
+                    echo "<td>".($out*10+$in)."</td>";
+                    $in++;
+                }
+            echo "</tr>";
+            $out++;
+        }
+        ?>
+    </table>
+</body>
+</html>
